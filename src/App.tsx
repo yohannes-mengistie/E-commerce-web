55cbe930feffb20d1d880c420a02c143f1b219d8
Yohannes Mengistie <yohannesmengistie634@gmail.com>
2025-02-19 15:20:52 +0300

    navbar prepare

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import React from 'react';
 import { Navbar } from './Components/Navbar/Navbar';
-
+import { BrowserRouter,Routes, Route} from 'react-router-dom';
+import { Shop } from './Page/Shop';
+import { ShopCatagory } from './Page/ShopCatagory';
 export default function App() {
   return (
     <div>
+      <BrowserRouter>
       <Navbar />
+      <Routes>
+         <Route path='/' element={<Shop />} />
+         <Route path='/mens' element={<ShopCatagory />} />
+         <Route path='/womens' element={<ShopCatagory />} />
+         <Route path='/kids' element={<ShopCatagory />} />
+      </Routes>
+      </BrowserRouter>
+      
     </div>
-  );
-}
+  )};
--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -4,6 +4,14 @@ import shopping from '../Assets/shopping.png'
 import logo from '../Assets/logo.jpg'
 
 export const Navbar = () => {
+  const [menu,setMenu] = React.useState("shop");
+  interface EventHandlerProps {
+    data: string;
+  }
+
+  function eventHandeler({ data }: EventHandlerProps): void {
+    setMenu(data);
+  }
   return (
     <div className={styles.navbar}>
         <div className={styles.navlogo}>
@@ -11,15 +19,16 @@ export const Navbar = () => {
           <p>SHOPPER</p>
         </div>
         <ul className={styles.navmenu}>
-          <li>Shop <hr/></li>
-          <li>Men</li>
-          <li>Women</li>
-          <li>Kids</li>
+          <li onClick={() => eventHandeler({ data: "shop" })}>Shop {menu === "shop" ? <hr/> :null}</li>
+          <li onClick={() => eventHandeler({ data: "men" })}>Men {menu === "men" ? <hr/> : null } </li>
+          <li onClick={() => eventHandeler({ data: "women" })}>Women {menu === "women" ?<hr/> : null}</li>
+          <li onClick={() => eventHandeler({ data: "kids" })}>Kids {menu === "kids" ? <hr/> : null}</li>
         </ul>
 
         <div className={styles.navlogincart}>
             <button>Login</button>
             <img src={shopping} alt="" />
+            <div className={styles.navcartcount}>0</div>
         </div>
 
         
